0caf36c6c292bc6250724312a20010b02d014eb9
owaisaquil-spec <owais.aquil@gmail.com>
2025-12-14 21:51:47 +0530

    Add files via upload

---
+++ starplus.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter no of rows:");
+    scanf("%d",&n);
+     int a = n/2 + 1;
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++){
+            if(i==a || j==a){
+            printf("*");
+            }
+            else {
+            printf(" ");
+            }
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
